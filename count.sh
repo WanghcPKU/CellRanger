c1cd10205e085fe513f640436710719617c0e003
WanghcPKU <165775424+WanghcPKU@users.noreply.github.com>
2024-07-08 12:37:47 +0800

    Create count.sh

---
+++ count.sh
@@ -0,0 +1,8 @@
+cellranger count \
+--id=AC \
+--localcores=20 \
+--localmem=400 \
+--expect-cells=10000 \
+--transcriptome=/lustre/user/taowlab/wanghc/work/AC/scrna/genome/celegan/ \
+--fastqs=/lustre/user/taowlab/wanghc/work/AC/scrna/raw/ \
+--sample=AC
